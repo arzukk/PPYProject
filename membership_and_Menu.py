7204b35af14ce8352f4074df7b10849056d1d88d
arzuk <93736885+s25701@users.noreply.github.com>
2023-05-21 17:58:11 +0200

    login page

--- membership_and_Menu.py
+++ membership_and_Menu.py
@@ -1,9 +1,18 @@
 import random
 import time
 import sqlite3
+import time
+from http.client import HTTPException
+from typing import List
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from fastapi import Response, RedirectResponse
 import lib
 from fastapi import FastAPI
 from pydantic import BaseModel
+from project import app
+
 
 class Membership():
     def __init__(self):
@@ -31,176 +40,45 @@ class Membership():
         list1 = self.cursor.fetchall()
 
         if len(list1) == 0:
-            print("There is no such account!")
-
+            return RedirectResponse(url="/login_failed")
         else:
-            if username == list1[0][0] and password == list1[0][1]:
-                print("Checking in.....")
-                time.sleep(3)
-                print("Application is opening please wait...")
-                time.sleep(3)
-                print("""
+            return RedirectResponse(url="/dashboard")
 
-                        ************************************************
-                             WELCOME TO LIBRARY APPLICATION            * 
-                                                                       *
-                        OPTIONS:                                       * 
-                                                                       * 
-                        1.) Show the all  books                        * 
-                                                                       * 
-                        2.) Query to book                              * 
-                                                                       * 
-                        3.) Add the book                               * 
-                                                                       * 
-                        4.) Delete the book                            * 
-                                                                       * 
-                        5.) Update the edition                         * 
-                                                                       *
-                        6.) Borrow the book                            *
-                                                                       *
-                        7.) Return the book                            *
-                                                                       *      
-                        8.) Settings of account                        *         
-                                                                       *                                          
-                        press 'q' for exit                             * 
-                                                                       * 
-                        ************************************************
-                        """)
+    @app.get("/login_failed", response_class=HTMLResponse)
+    def login_failed(self):
+        return """
+            <html>
+            <head>
+                <title>Login Failed</title>
+            </head>
+            <body>
+                <h1>Login Failed</h1>
+                <p>There is no such account!</p>
+            </body>
+            </html>
+        """
 
-                while True:
-                    action = input("Select your action you want to do in App: ")
-                    if action == "q":
-                        print("The Application is closing...")
-                        time.sleep(4)
-                        print("The Application is closed....")
-                        break
-
-                    elif action == "1":
-                        book = lib.Library()
-                        print("*************All the Books**************")
-                        time.sleep(3)
-                        book.Show_ALL_Books()
-
-
-                    elif action == "2":
-                        book1 = lib.Library()
-                        book2 = input("Please enter which book  you  want:  ")
-                        print("Please waiting.....")
-                        time.sleep(3)
-                        book1.Query_To_Book(book2)
-
-
-                    elif action == "3":
-
-                        book3 = lib.Library()
-
-                        print("Enter the information of the book you want to add to the library.")
-                        time.sleep(1)
-                        book4 = input("Please enter the book name:")
-                        writer = input("Please enter the writer:")
-                        publisher = input("Please enter the publisher:")
-                        type = input("Please enter the type:")
-                        number_of_page = input("Please enter the number of page:")
-                        edition = input("Please enter the edition:")
-
-                        print("Adding.....")
-                        time.sleep(2)
-                        book3.Add_To_Book(book4, writer, publisher, type, number_of_page, edition)
-                        print("The book has been added your library")
-
-                    elif action == "4":
-
-                        book5 = lib.Library()
-                        book6 = input("Which book do you want to the delete? Please Enter:")
-
-                        print("Being deleted...")
-                        book5.Delete_Book(book6)
+    @app.get("/dashboard", response_class=HTMLResponse)
+    def dashboard(self):
+        return """
+            <html>
+            <head>
+                <title>Dashboard</title>
+            </head>
+            <body>
+                <h1>Dashboard</h1>
+                <!-- Add your dashboard content here -->
+            </body>
+            </html>
+        """
 
 
 
-                    elif action == "5":
-                        book7 = lib.Library()
-                        book8 = input("Which book do you want to update the edition? Please Enter:")
-                        print("Updating...")
-                        time.sleep(3)
-                        book7.Update_Edition(book8)
-
-                    elif action == "6":
-                        book9 = lib.Library()
-                        book10 = input("Enter the title of the book you want to borrow: ")
-                        deadline = input("Enter the deadline for returning the book (yyyy-mm-dd):")
-                        print("Borrowing...")
-                        time.sleep(3)
-                        book9.Borrow_Book(book10, deadline)
-
-                    elif action == "7":
-                        book9 = lib.Library()
-                        book10 = input("Enter the title of the book you want to return: ")
-                        print("Returning...")
-                        time.sleep(3)
-                        book9.Return_Book(book10)
-
-                    elif action == "8":
-                        print("""
-                        ************************************************
-                        
-                            Menu Settings:
-                            
-                            1.) Delete Account 
-                            
-                            2.) Change Username
-                        
-                            3.) Change Password
-                        
-                            press 'q' for exit
-                            
-                        *************************************************    
-                            
-                            
-                        """)
-                        while True:
-                            action = input("Select your action you want to do in settings: ")
-                            if action == "q":
-                                print("The Application is closing...")
-                                time.sleep(4)
-                                print("the Application is closed....")
-                                break
-
-                            elif action == "1":
-                                member = Membership()
-
-                                name = input("Enter your username:")
-                                email = input("Enter your email:")
-
-                                member.Delete_Account(name, email)
-
-                            elif action == "2":
-                                member1 = Membership()
-
-                                name1 = input("Please enter your username:")
-                                new_name = input("Please enter your new username:")
-
-                                member1.Change_Username(name1, new_name)
-
-
-                            elif action == "3":
-                                member2 = Membership()
-
-                                name2 = input("Please enter your username:")
-                                password1 = input("Please enter your password:")
-                                password2 = input("Please enter your password:")
-
-                                member2.Change_Password(name2, password1, password2)
 
 
 
-                            else:
-                                print("You select invalid action! Please enter correct action")
 
 
-
-                    else:
-                        print("You select invalid action! Please enter correct action")
 
     def Delete_Account(self, name, email):
         query = "SELECT * FROM Members WHERE Username = ? and Email = ?"
--- project.py
+++ project.py
@@ -1,100 +1,114 @@
 import time
+from fastapi.exceptions import HTTPException
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from pydantic import BaseModel
-
-import membership_and_Menu
-
-app = FastAPI()
-
 
 class LoginRequest(BaseModel):
     username: str
     password: str
 
+class Login:
+    def __init__(self):
+        self.logged_in = False
 
-class AccountCreateRequest(BaseModel):
-    username: str
-    password: str
-    email: str
+    def login(self, username: str, password: str) -> bool:
+        if username == "admin" and password == "password":
+            self.logged_in = True
+        else:
+            self.logged_in = False
+        return self.logged_in
 
+    def is_logged_in(self) -> bool:
+        return self.logged_in
 
-class PasswordRecoveryRequest(BaseModel):
-    username: str
-    email: str
+login = Login()
+app = FastAPI()
 
+@app.get("/")
+def home():
+    return RedirectResponse(url="/login")
+
+@app.get("/login", response_class=HTMLResponse)
+def show_login_form():
+    return """
+        <html>
+        <head>
+            <title>Login Page</title>
+        </head>
+        <body>
+            <h1>Login</h1>
+            <form action="/login" method="post" onsubmit="event.preventDefault(); login()">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required><br>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required><br>
+                <input type="submit" value="Log In">
+            </form>
+            <script>
+                function login() {
+                    const form = document.querySelector('form');
+                    const formData = new FormData(form);
+                    const data = Object.fromEntries(formData.entries());
+                    fetch('/login', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(data),
+                    })
+                    .then(response => response.json())
+                    .then(result => {
+                        if (result.message === 'Login successful!') {
+                            window.location.href = '/dashboard';
+                        } else {
+                            alert('Login failed!');
+                        }
+                    })
+                    .catch(error => console.error(error));
+                }
+            </script>
+        </body>
+        </html>
+    """
 
 @app.post("/login")
-def login(request: LoginRequest):
-    member = membership_and_Menu.Membership()
-    result = member.Login_Account(request.username, request.password)
-    return {"status": result}
-
-
-@app.post("/create_account")
-def create_account(request: AccountCreateRequest):
-    member = membership_and_Menu.Membership()
-    member.Create_Account(request.username, request.password, request.email)
-    return {"status": "success"}
-
-
-@app.post("/password_recovery")
-def password_recovery(request: PasswordRecoveryRequest):
-    member = membership_and_Menu.Membership()
-    member.Recovery_Code(request.username, request.email)
-    return {"status": "success"}
+def login_user(request: LoginRequest):
+    if login.login(request.username, request.password):
+        return {"message": "Login successful!"}
+    else:
+        raise HTTPException(status_code=401, detail="Login failed!")
 
 
 if __name__ == "__main__":
-    print("""
-    ***************************************************** 
-                                                        *
-          WELCOME TO THE LIBRARY APPLICATION LOGIN      *
-                                                        *
-    1.) Login your account                              *    
-                                                        *
-    2.) Create a new account                            *
-                                                        *
-    3.) Forget my password                              *
-                                                        *
-                                                        *
-        press 'q' for exit                              *    
-                                                        *
-                                                        *
-    *****************************************************
-    """)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
-    while True:
-        action = input("Select your action you want to do: ")
-        if action == "q":
-            print("The Application is closing...")
-            time.sleep(4)
-            print("the Application is closed....")
-            break
 
-        elif action == "1":
-            username = input("please enter your username:")
-            password = input("please enter your password:")
-            login_request = LoginRequest(username=username, password=password)
-            login_result = login(login_request)
-            print(login_result)
 
-        elif action == "2":
-            print("Welcome new member! Please enter your information")
-            time.sleep(3)
-            username = input("Enter your username:")
-            password = input("Enter your password:")
-            email = input("Enter your email:")
-            account_create_request = AccountCreateRequest(username=username, password=password, email=email)
-            create_account(account_create_request)
-            print("Your information saved successfully. Welcome again")
 
-        elif action == "3":
-            username = input("Please enter your username:")
-            email = input("Please enter your email:")
-            password_recovery_request = PasswordRecoveryRequest(username=username, email=email)
-            password_recovery(password_recovery_request)
-            print("A password recovery code has been sent to your email. Please check your email")
 
-        else:
-            print("You select an invalid action! Please enter a correct action")
+#
+# class AccountCreateRequest(BaseModel):
+#     username: str
+#     password: str
+#     email: str
+#
+#
+# class PasswordRecoveryRequest(BaseModel):
+#     username: str
+#     email: str
+#
+# @app.post("/create_account")
+# def create_account(request: AccountCreateRequest):
+#     member = membership_and_Menu.Membership()
+#     member.Create_Account(request.username, request.password, request.email)
+#     return {"status": "success"}
+#
+#
+# @app.post("/password_recovery")
+# def password_recovery(request: PasswordRecoveryRequest):
+#     member = membership_and_Menu.Membership()
+#     member.Recovery_Code(request.username, request.email)
+#     return {"status": "success"}
