7cdf00e4026c0f0efd4b69e1aeeb770e13baeff9
Enes Catal <catalenes6@gmail.com>
2023-06-01 18:50:16 +0200

    addedQueryBook htmls and seperate fileforshowbooks

--- PPYProject-arzununki/project.py
+++ PPYProject-arzununki/project.py
@@ -3,7 +3,7 @@ import random
 from fastapi import FastAPI, Request, HTTPException, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class LoginRequest(BaseModel):
     username: str
@@ -16,6 +16,9 @@ class CreateUserRequest(BaseModel):
 
 class CreateDashboardRequest(BaseModel):
     pass
+
+class QueryBookRequest(BaseModel):
+    book_name: str = Field(..., min_length=1)
 
 class Login:
     def __init__(self):
@@ -60,7 +63,41 @@ class Login:
         connection.close()
 
 
+class BookDatabase:
+    def __init__(self):
+        self.create_connection()
+
+    def create_connection(self):
+        self.connection = sqlite3.connect("Books.db")
+        self.cursor = self.connection.cursor()
+        query = "CREATE TABLE IF NOT EXISTS Books (Name TEXT,Writer TEXT,Publisher TEXT,Type TEXT,Number_of_Pages INT,Edition INT)"
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def finish_connection(self):
+        self.connection.close()
+
+    def get_all_books(self):
+        connection = sqlite3.connect("Books.db")
+        cursor = connection.cursor()
+        query = "SELECT * FROM Books"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        connection.close()
+        return result
+
+    def query_book(self, book_name: str):
+        connection = sqlite3.connect("Books.db")
+        cursor = connection.cursor()
+        query = "SELECT * FROM Books WHERE Name = ?"
+        cursor.execute(query, (book_name,))
+        result = cursor.fetchall()
+        connection.close()
+        return result
+
+
 login = Login()
+book_db = BookDatabase()
 app = FastAPI()
 templates = Jinja2Templates(directory="C:\\Users\\ENES CATAL\\Downloads\\PPYProject-arzununki\\PPYProject-arzununki\\templates")
 
@@ -74,7 +111,7 @@ def show_login_form(request: Request):
 
 @app.get("/dashboard", response_class=HTMLResponse)
 def show_dashboard(request: Request):
-    return templates.TemplateResponse("dashboard.html", {"request": request})
+    return templates.TemplateResponse("dashboard.html", {"request": request, "logged_in": login.is_logged_in()})
 
 @app.post("/login")
 def login_user(request: LoginRequest):
@@ -96,6 +133,24 @@ def create_account(request: CreateUserRequest):
     login.create_account(request.username, request.password, request.email)
     return {"message": "Account created!"}
 
+@app.get("/show-books", response_class=HTMLResponse)
+def show_books(request: Request):
+    books = book_db.get_all_books()
+    return templates.TemplateResponse("showBooks.html", {"request": request, "books": books})
+
+@app.get("/query-book", response_class=HTMLResponse)
+def show_query_book_form(request: Request):
+    return templates.TemplateResponse("queryBook.html", {"request": request})
+
+@app.post("/query-book", response_class=HTMLResponse)
+def query_book(request: QueryBookRequest):
+    book_name = request.book_name
+    books = book_db.query_book(book_name)
+    if books:
+        return templates.TemplateResponse("queryBookResult.html", {"request": request, "books": books})
+    else:
+        return templates.TemplateResponse("queryBookResult.html", {"request": request, "message": "No books found with the given name."})
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=8000)
